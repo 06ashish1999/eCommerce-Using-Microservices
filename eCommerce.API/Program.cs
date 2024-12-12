92e56719c23ee7d5dbabbeefa31f49630388cb60
Sai Ashish Pinnamsetty <ideas.something@gmail.com>
2024-12-12 16:14:55 +0530

    by defualt asp.net core unable to convert from enum to string so we are adding convertors

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,6 +1,7 @@
 using eCommerce.API.Middlewares;
 using eCommerce.Core;
 using eCommerce.Infrastructure;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,11 @@ builder.Services.AddInfrastructure();
 builder.Services.AddTransient<ExceptionHandling>();
 
 // adding controllers as service
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    // by default asp.net unable to convert enum to string so we need to add converter
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 var app = builder.Build();
 
